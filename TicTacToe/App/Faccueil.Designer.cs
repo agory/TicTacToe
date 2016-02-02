d25a7da6dc95a8ea6374c6dc8e36b17104e8f061
agory <gory.alexandre02@gmail.com>
2016-02-02 08:30:50 +0100

    Init

---
+++ TicTacToe/App/Faccueil.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace App
+{
+    partial class FAccueil
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recursifToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itératifToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.recursifToolStripMenuItem,
+            this.itératifToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // recursifToolStripMenuItem
+            // 
+            this.recursifToolStripMenuItem.Name = "recursifToolStripMenuItem";
+            this.recursifToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.recursifToolStripMenuItem.Text = "Recursif";
+            // 
+            // itératifToolStripMenuItem
+            // 
+            this.itératifToolStripMenuItem.Name = "itératifToolStripMenuItem";
+            this.itératifToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.itératifToolStripMenuItem.Text = "Itératif";
+            // 
+            // FAccueil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FAccueil";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FAccueil_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recursifToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itératifToolStripMenuItem;
+    }
+}
+
---
+++ TicTacToe/App/Faccueil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class FAccueil : Form
+    {
+        private int method;
+        private int[,] matrice;
+
+        public FAccueil()
+        {
+            method = 0;
+            matrice = new int[3, 3];
+            InitializeComponent();
+        }
+
+        private void FAccueil_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void recursif() {
+
+        }
+        private void iteratif()
+        {
+
+        }
+    }
+}
