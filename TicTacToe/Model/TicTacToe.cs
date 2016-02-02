755babbee8faaaf7913b2b60fd14d340334fba95
agory <gory.alexandre02@gmail.com>
2016-02-02 21:06:51 +0100

    computeWin

--- TicTacToe/Model/TicTacToe.cs
+++ TicTacToe/Model/TicTacToe.cs
@@ -14,7 +14,34 @@ namespace Model
             this.matrix = matrix;
         }
 
-        
+        public void computeRecursiveChoice() {
+            if (matrix[1, 1] == 0)
+            {
+                matrix[1, 1] = 2;
+            }
+            else {
+                
+            }
+        }
 
+        private int computeWin(int[,] matrix) {
+            int winner = 0;
+            for (int i = 0; i < 3; i++) {
+                if ((matrix[i, 0] == matrix[i, 1] && matrix[i, 0] == matrix[i, 2]) ||
+                   (matrix[0, i] == matrix[1, i] && matrix[0, i] == matrix[2, i]))
+                {
+                    winner = matrix[i, i];
+                }
+            }
+            if (winner == 0) {
+                if ((matrix[0, 0] == matrix[1, 1] && matrix[2, 2] == matrix[1, 1]) ||
+                   (matrix[3, 0] == matrix[1, 1] && matrix[1, 1] == matrix[0, 2]))
+                {
+                    winner = matrix[1, 1];
+                }
+            }
+            return winner;
+        }
+        
     }
 }
